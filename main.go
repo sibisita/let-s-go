2e7dfabbe8d1a2fba0a26303bd14d58a90c3c18c
Sibi <sibi.c_b@nokia.com>
2026-01-15 13:48:40 +0530

    2.4 Customizing HTTP headers

--- main.go
+++ main.go
@@ -18,6 +18,13 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Create a new Snippet..."))
 }
 
@@ -25,7 +32,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create/", snippetCreate)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
