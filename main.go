c76a8565e9abd80967b48bf08ea1ff85420c2406
Sibi <sibi.c_b@nokia.com>
2026-01-15 14:10:32 +0530

    2.5 URL query strings

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display specific content......"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %v ...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
