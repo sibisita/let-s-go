56c078ca5cce3d7ad8b67636dbea1c0374cde365
Sibi <sibi.c_b@nokia.com>
2026-01-17 21:53:06 +0530

    3.2 Leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,14 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address port")
 	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errLog := log.New(os.Stderr, "ERROR\t", log.Lshortfile|log.Ltime|log.Ldate)
 	mux := http.NewServeMux()
 	// File Server
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -20,7 +23,7 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// web server
-	log.Printf("Starting server on %v", *addr)
+	infoLog.Printf("Starting server on %v", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errLog.Fatal(err)
 }
