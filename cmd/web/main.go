3b9e58acbbf271dd12b0c4e5f7a6f176d225379e
Sibi <sibi.c_b@nokia.com>
2026-01-17 21:12:55 +0530

    3.1 Managing configuration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address port")
+	flag.Parse()
 	mux := http.NewServeMux()
 	// File Server
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -17,7 +20,7 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// web server
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %v", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
