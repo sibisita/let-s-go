330c7b2697efa4f219c161fa36fb25699567e1eb
Sibi <sibi.c_b@nokia.com>
2026-01-17 22:37:30 +0530

    3.3 Dependency injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,20 +7,29 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address port")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stderr, "ERROR\t", log.Lshortfile|log.Ltime|log.Ldate)
+	app := &application{
+		errorLog: errLog,
+		infoLog:  infoLog,
+	}
 	mux := http.NewServeMux()
 	// File Server
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// routing
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	// web server
 	infoLog.Printf("Starting server on %v", *addr)
